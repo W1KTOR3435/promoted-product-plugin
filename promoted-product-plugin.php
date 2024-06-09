e752529d322d27533df716702fb61909a2ec9966
Wiktor <gres3435@wp.pl>
2024-06-09 22:58:05 +0200

    feat: Added check before init
    Added:
    - check if WooCommerce is active before initializing plugin.

--- promoted-product-plugin.php
+++ promoted-product-plugin.php
@@ -3,15 +3,29 @@
 Plugin Name: Promoted Product Plugin
 Description: A plugin to feature a promoted product on every page.
 Version: 1.0
-Author: Wiktor
+Author: Wiktor Kowalczyk
 */
 
 if ( ! defined( 'ABSPATH' ) ) {
     exit;
 }
 
-// Autoload classes
-require_once __DIR__ . '/vendor/autoload.php';
+function vm_is_woocommerce_active() {
+    include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+    return is_plugin_active( 'woocommerce/woocommerce.php' );
+}
 
-// Initialize the plugin
-PromotedProduct\Plugin::init();
+function vm_admin_notice_wc_inactive() {
+    ?>
+    <div class="notice notice-error">
+        <p><?php esc_html_e( 'The Promoted Product Plugin requires WooCommerce to be installed and activated.', 'promoted-product' ); ?></p>
+    </div>
+    <?php
+}
+
+if ( vm_is_woocommerce_active() ) {
+    require_once __DIR__ . '/vendor/autoload.php';
+    PromotedProduct\Plugin::init();
+} else {
+    add_action( 'admin_notices', 'vm_admin_notice_wc_inactive' );
+}
