a13bb3732faa618e1983059d53833aca86b19889
Wiktor <gres3435@wp.pl>
2024-06-09 23:38:15 +0200

    docs: Added docs to main plugin class

--- src/Plugin.php
+++ src/Plugin.php
@@ -1,11 +1,25 @@
 <?php
+/**
+ * Main plugin class for the Promoted Product Plugin.
+ *
+ * @package PromotedProduct
+ */
 
 namespace PromotedProduct;
 
+/**
+ * Main plugin class.
+ */
 class Plugin {
-    public static function init() {
-        Settings::init();
-        ProductEditor::init();
-        Frontend::init();
-    }
+
+	/**
+	 * Initialize the plugin.
+	 *
+	 * @return void
+	 */
+	public static function init() {
+		Settings::init();
+		ProductEditor::init();
+		Frontend::init();
+	}
 }
