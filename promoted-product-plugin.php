feaa1e2a4116e6ff0ef3d68e097c5dca9c3f14c6
Wiktor <gres3435@wp.pl>
2024-06-09 23:31:44 +0200

    style: Plugin main file

--- promoted-product-plugin.php
+++ promoted-product-plugin.php
@@ -1,20 +1,28 @@
 <?php
-/*
-Plugin Name: Promoted Product Plugin
-Description: A plugin to feature a promoted product on every page.
-Version: 1.0
-Author: Wiktor Kowalczyk
-*/
+/**
+ * Plugin Name: Promoted Product Plugin
+ * Description: A plugin to feature a promoted product on every page.
+ * Version: 1.0
+ * Author: Wiktor Kowalczyk
+ */
 
 if ( ! defined( 'ABSPATH' ) ) {
-    exit;
+    exit; // Exit if accessed directly
 }
 
+/**
+ * Check if WooCommerce is active.
+ *
+ * @return bool
+ */
 function vm_is_woocommerce_active() {
-    include_once( ABSPATH . 'wp-admin/includes/plugin.php' );
+    include_once ABSPATH . 'wp-admin/includes/plugin.php';
     return is_plugin_active( 'woocommerce/woocommerce.php' );
 }
 
+/**
+ * Display an admin notice if WooCommerce is inactive.
+ */
 function vm_admin_notice_wc_inactive() {
     ?>
     <div class="notice notice-error">
@@ -23,9 +31,12 @@ function vm_admin_notice_wc_inactive() {
     <?php
 }
 
+// Check if WooCommerce is active
 if ( vm_is_woocommerce_active() ) {
+    // Autoload dependencies and initialize the plugin
     require_once __DIR__ . '/vendor/autoload.php';
     PromotedProduct\Plugin::init();
 } else {
+    // Show admin notice if WooCommerce is inactive
     add_action( 'admin_notices', 'vm_admin_notice_wc_inactive' );
 }
